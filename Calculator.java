ad37a559e73767b3d58d86ac7c5d1df68e59b02a
pradeepa <pradeeparaj2510@gmail.comgit config --global user.name pradeepa>
2022-06-21 16:49:55 +0530

    commiting the changes after resolving

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,13 @@ public class Calculator{
     public int product(int a, int b){
         return a*b;
     }
+<<<<<<< Updated upstream
   public int diff(int a,int b){
     return a-b;
   }
+=======
+    public int divide(int a, int b){
+        return a*b;
+    }
+>>>>>>> Stashed changes
 }
